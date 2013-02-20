a862774337275a08152e9e318424588b219d51d1
Ryan Eastridge <ryan@eastridge.me>
2013-02-20 13:06:36 -0800

    Ensure public is created

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,8 @@ module.exports = function(grunt) {
       lumbarFile = './lumbar.json',
       hostname = require('os').hostname();
   
+  grunt.file.mkdir(publicDir);
+
   grunt.initConfig({
     // create a static webserver
     connect: {
