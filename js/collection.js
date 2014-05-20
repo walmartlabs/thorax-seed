ab0d29ee2507bf2b2f8f653eb8ced9024f8e4ffb
kpdecker <kpdecker@gmail.com>
2014-05-20 10:04:54 -0500

    Implement hack for $serverCache read

--- js/collection.js
+++ js/collection.js
@@ -1,3 +1,12 @@
 Application.Collection = Thorax.Collection.extend({
-
+  sync: function(method, model, options) {
+    // WARN: This is a quick hack for the purposes of the demo.
+    // https://github.com/walmartlabs/thorax/issues/371 has been filed to create a better impl
+    var url = this.url();
+    if (method === 'read' && window.$serverCache && $serverCache[url]) {
+      options.success($serverCache[url]);
+    } else {
+      return Thorax.Collection.prototype.sync.apply(this, arguments);
+    }
+  }
 });
